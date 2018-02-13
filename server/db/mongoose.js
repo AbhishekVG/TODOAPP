704b503fd2498b916b74cf36c883d8a4218e60ba
Abhishek B V <VAbhishek@ESTUATE.COM>
2018-02-13 12:22:27 +0530

    made mongoose url to be passed thru process.env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect("mongodb://todoappDB:password1234@ds125578.mlab.com:25578/todoapp");
-
+mongoose.connect(process.env.MONGODB_URI);
+// console.log('MONGODB_URI', process.env.MONGODB_URI)
+// mongoose.connect("mongodb://todoappDB:password1234@ds125578.mlab.com:25578/todoapp");
 module.exports = {mongoose};
